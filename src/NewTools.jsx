1f5005fe3741a5267b75e20136c5cfafbc937c21
Guilherme Bittencourt Corrêa <guilhermebittencourtcorrea@gmail.com>
2023-02-24 15:35:51 -0300

    📱 responsive

--- src/NewTools.jsx
+++ src/NewTools.jsx
@@ -39,16 +39,16 @@ export default function NewTools() {
         <>
       <form className="m-5 flex">
             <div className="ml-5" >
-            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool title' onChange={(e)=> setTitle(e.target.value)}></input>
+            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 text-white">Title</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool title' onChange={(e)=> setTitle(e.target.value)}></input>
         </div>
         <div className="ml-5" >
-            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool Body' onChange={(e)=> setBody(e.target.value)}></input>
+            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 text-white">Body</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool Body' onChange={(e)=> setBody(e.target.value)}></input>
         </div>
         <div className="ml-5" >
-            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Link</label>
-            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool Link' onChange={(e)=> setLink(e.target.value)}></input>
+            <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 text-white">Link</label>
+            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Tool Link' onChange={(e)=> setLink(e.target.value)}></input>
         </div>
 <button className="mt-7 ml-5 inline-flex items-center justify-center h-10 gap-2 px-5 text-sm font-medium tracking-wide text-white transition duration-300 rounded focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none" onClick={handleSubmit}>
   <span >Add</span>
